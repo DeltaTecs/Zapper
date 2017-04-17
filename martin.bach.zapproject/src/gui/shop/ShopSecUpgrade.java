f3edbae3e510044c97e8236ee73fff0da52b0276
DeltaTecs <delta-tecs@web.de>
2017-04-17 20:07:03 +0200

    Stat-Upgrade in shop fully available

--- martin.bach.zapproject/src/gui/shop/ShopSecUpgrade.java
+++ martin.bach.zapproject/src/gui/shop/ShopSecUpgrade.java
@@ -10,6 +10,7 @@ import java.awt.RenderingHints;
 import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 
+import corecase.Cmd;
 import corecase.MainZap;
 import io.TextureBuffer;
 
@@ -20,8 +21,9 @@ public abstract class ShopSecUpgrade {
 	private static final int SYM_BACK_X = 40;
 	private static final int SYM_BACK_Y = 70;
 	private static final int STAT_POS_Y = 140;
-	private static final int STAT_SPACE_Y = 10;
+	private static final int STAT_SPACE_Y = 15;
 	private static final Color COLOR_HOVER_BACK = new Color(0, 0, 50, 30);
+	private static final Color COLOR_HOVER_BUY = new Color(255, 0, 0, 60);
 	private static final Color COLOR_GREEN = new Color(30, 205, 30);
 	private static final Font FONT_BALANCE = new Font("Arial", Font.BOLD, 35);
 	private static final Font FONT_VALUE = new Font("Arial", 0, 20);
@@ -30,19 +32,25 @@ public abstract class ShopSecUpgrade {
 	private static final Font FONT_PRICE = new Font("Arial", Font.BOLD, 26);
 	private static final Font FONT_UPGRADE = new Font("Arial", Font.BOLD, 33);
 	private static final Stroke STROKE_UPGRADE = new BasicStroke(3);
+	private static final Stroke STROK_TOPIC_FRAME = new BasicStroke(1.4f);
 	private static final Rectangle BOUNDS_BACK = new Rectangle(SYM_BACK_X - 5, SYM_BACK_Y - 5, 60, 60);
+	private static final Rectangle[] BOUNDS_BUY = new Rectangle[] {
+			new Rectangle(STAT_POS_X + 490, STAT_POS_Y + (0 * 40) + (0 * STAT_SPACE_Y) + 2, 76, 36),
+			new Rectangle(STAT_POS_X + 490, STAT_POS_Y + (1 * 40) + (1 * STAT_SPACE_Y) + 2, 76, 36),
+			new Rectangle(STAT_POS_X + 490, STAT_POS_Y + (2 * 40) + (2 * STAT_SPACE_Y) + 2, 76, 36),
+			new Rectangle(STAT_POS_X + 490, STAT_POS_Y + (3 * 40) + (3 * STAT_SPACE_Y) + 2, 76, 36) };
 	private static final Polygon SYMBOL_BACK = new Polygon(new int[] { SYM_BACK_X, 50 + SYM_BACK_X, 50 + SYM_BACK_X },
 			new int[] { 25 + SYM_BACK_Y, SYM_BACK_Y, 50 + SYM_BACK_Y }, 3);
 	private static final String TEXT_UPGRADE = "BUY";
-
 	// -- Hovers -------
 	private static boolean hoveringBack = false;
+	private static boolean[] hoveringBuys = new boolean[] { false, false, false, false };
 	// ---
 
 	// -- Mechanics -----
 	// Health, Damage, Ammo-Efficiency, Proj.Speed
 	private static byte[] activeUpgrades = new byte[] { 0, 0, 0, 0 };
-	private static int[] priceTable = new int[] { 100, 200, 400, 600, 1300};
+	private static int[] priceTable = new int[] { 100, 200, 400, 600, 1300 };
 	// ---
 
 	protected static void update() {
@@ -82,25 +90,25 @@ public abstract class ShopSecUpgrade {
 
 		int y = STAT_POS_Y;
 		paintStatObject(g, STAT_POS_X, y, PaintableStats.IMG_HEALTH, PaintableStats.COLOR_HEALTH,
-				"health: " + MainZap.getPlayer().getMaxHp() + " hp", activeUpgrades[0], false);
+				"health: " + MainZap.getPlayer().getMaxHp() + " hp", activeUpgrades[0], hoveringBuys[0]);
 		y += 40 + STAT_SPACE_Y;
 		paintStatObject(g, STAT_POS_X, y, PaintableStats.IMG_DAMAGE, PaintableStats.COLOR_DAMAGE,
-				"damage: " + MainZap.getPlayer().getBulletDamage() + " hp", activeUpgrades[1], false);
+				"damage: " + MainZap.getPlayer().getBulletDamage() + " hp", activeUpgrades[1], hoveringBuys[1]);
 		y += 40 + STAT_SPACE_Y;
 		paintStatObject(g, STAT_POS_X, y, PaintableStats.IMG_EFFICIENCY, PaintableStats.COLOR_EFFICIENCY,
-				"ammo usage: " + MainZap.getPlayer().getAmmoUsageFac() + "x", activeUpgrades[2], false);
+				"ammo usage: " + MainZap.getPlayer().getAmmoUsageFac() + "x", activeUpgrades[2], hoveringBuys[2]);
 		y += 40 + STAT_SPACE_Y;
 		paintStatObject(g, STAT_POS_X, y, PaintableStats.IMG_BULLETSPEED, PaintableStats.COLOR_BULLETSPEED,
-				"bullet speed: " + MainZap.getPlayer().getBulletSpeed() + " p/t", activeUpgrades[3], false);
+				"bullet speed: " + MainZap.getPlayer().getBulletSpeed() + " p/t", activeUpgrades[3], hoveringBuys[3]);
 
 	}
 
 	private static void paintStatObject(Graphics2D g, int x, int y, BufferedImage img, Color c, String text, byte lvl,
 			boolean hovered) {
 
-		g.setStroke(new BasicStroke(1));
-		g.setColor(Color.BLUE);
-		// g.drawLine(x, y + 20, x + 600, y + 20);
+		g.setColor(Color.BLACK);
+		g.setStroke(STROK_TOPIC_FRAME);
+		g.drawRect(x - 3, y - 3, 575, 46);
 
 		g.drawImage(img, x, y, 40, 40, null);
 		g.setColor(c);
@@ -108,7 +116,7 @@ public abstract class ShopSecUpgrade {
 		g.drawString(text, x + 50, y + FONT_VALUE.getSize() + 8);
 		g.setColor(Color.BLACK);
 		g.setFont(FONT_LVL);
-		g.drawString(lvl + "/5", x + 250, y + FONT_LVL.getSize() + 5);
+		g.drawString(lvl + "/5", x + 254, y + FONT_LVL.getSize() + 5);
 
 		if (lvl == 5)
 			return; // Ausgemaxt. Man kann eh nix kaufen
@@ -117,7 +125,7 @@ public abstract class ShopSecUpgrade {
 		g.setColor(COLOR_GREEN);
 		if (img == PaintableStats.IMG_EFFICIENCY) {
 			// Minus. Es geht um den Munitions-Verbrauch
-			g.drawString("-10%", x + 310, y + FONT_PERCENTAGE.getSize() + 7);
+			g.drawString("-10%", x + 314, y + FONT_PERCENTAGE.getSize() + 7);
 		} else {
 			g.drawString("+10%", x + 310, y + FONT_PERCENTAGE.getSize() + 7);
 		}
@@ -134,6 +142,10 @@ public abstract class ShopSecUpgrade {
 		}
 
 		g.setColor(Color.BLACK);
+		g.setStroke(STROK_TOPIC_FRAME);
+		g.drawRect(x + 380, y - 3, 192, 46);
+
+		g.setColor(Color.BLACK);
 		g.setFont(FONT_PRICE);
 		g.drawString(priceTable[lvl] + "", x + 411, y + FONT_PRICE.getSize() + 4);
 
@@ -141,7 +153,7 @@ public abstract class ShopSecUpgrade {
 		g.setStroke(STROKE_UPGRADE);
 		g.drawRect(x + 490, y + 2, 76, 36);
 		if (hovered) {
-			g.setColor(ShopSecBuy.COLOR_BUY_HOVER);
+			g.setColor(COLOR_HOVER_BUY);
 			g.fillRect(x + 490, y + 2, 76, 36);
 		}
 		g.setColor(ShopSecBuy.COLOR_BUY);
@@ -158,6 +170,16 @@ public abstract class ShopSecUpgrade {
 			return;
 		}
 
+		// Irgend ein "Buy" geklickt?
+		int i = 0;
+		for (Rectangle r : BOUNDS_BUY) {
+			if (r.contains(tx, ty)) {
+				purchase(i); // Versuch zu kaufen
+				return;
+			}
+			i++;
+		}
+
 	}
 
 	protected static void callMove(int tx, int ty) {
@@ -167,8 +189,56 @@ public abstract class ShopSecUpgrade {
 			return;
 		}
 
+		// Maus über "Buy" bei Stats?
+		int i = 0;
+		hoveringBuys[0] = false;
+		hoveringBuys[1] = false;
+		hoveringBuys[2] = false;
+		hoveringBuys[3] = false;
+		for (Rectangle r : BOUNDS_BUY) {
+			if (r.contains(tx, ty)) {
+				hoveringBuys[i] = true;
+				return;
+			}
+			i++;
+		}
+
+		hoveringBuys[0] = false;
+		hoveringBuys[1] = false;
+		hoveringBuys[2] = false;
+		hoveringBuys[3] = false;
 		hoveringBack = false;
 
+	}
+
+	private static void purchase(int index) {
+		if (priceTable[index] > MainZap.getCrystals() || activeUpgrades[index] == 5)
+			return; // Nicht genug Knete oder alle schon gekauft
+
+		MainZap.setCrystals(MainZap.getCrystals() - priceTable[index]);
+		activeUpgrades[index]++;
+
+		switch (index) {
+		case 0:
+			MainZap.getPlayer()
+					.setMaxHp(MainZap.getPlayer().getMaxHp() + (int) (MainZap.getPlayer().getMaxHp() * 0.1f));
+			break;
+		case 1:
+			MainZap.getPlayer().setBulletDamage(
+					MainZap.getPlayer().getBulletDamage() + (int) (MainZap.getPlayer().getBulletDamage() * 0.1f));
+			break;
+		case 2:
+			MainZap.getPlayer().setAmmoUsageFac(
+					MainZap.getPlayer().getAmmoUsageFac() - (MainZap.getPlayer().getAmmoUsageFac() * 0.1f));
+			break;
+		case 3:
+			MainZap.getPlayer().setBulletSpeed(
+					MainZap.getPlayer().getBulletSpeed() + (int) (MainZap.getPlayer().getBulletSpeed() * 0.1f));
+			break;
+		default:
+			Cmd.err("ShopSecUpgrade:231 Impossible!");
+			break;
+		}
 	}
 
 	protected static void reset() {
